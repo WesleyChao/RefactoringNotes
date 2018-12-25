d0f2a058b0653b2458e2502ec84e1db98755d5c6
wesley <1443997117@qq.com>
2018-12-25 22:07:51 +0800

    init

---
+++ Refactoring_Chapter1/src/Rental.java
@@ -0,0 +1,19 @@
+public class Rental {
+
+    private Movie _movie;
+    private int _dayRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _dayRented = daysRented;
+    }
+
+    public int getDayRented() {
+        return _dayRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+}
