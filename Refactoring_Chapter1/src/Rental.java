1da7c06c7b0e814cfa32ad03e896c71dab8f78d1
wesley <1443997117@qq.com>
2018-12-26 23:07:01 +0800

    18-12-26

--- Refactoring_Chapter1/src/Rental.java
+++ Refactoring_Chapter1/src/Rental.java
@@ -15,5 +15,26 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
-
+      double getCharge(){
+        double result = 0;
+        // determine amounts for each line
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDayRented() > 2) {
+                    result += (getDayRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDayRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDayRented() > 3) {
+                    result += (getDayRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return  result;
+    }
 }
